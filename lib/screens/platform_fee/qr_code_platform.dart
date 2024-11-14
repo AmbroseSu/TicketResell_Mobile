6203c1bae0d530b6847e6a1cb4c511b33e31bf38
AmbroseSu <ambrose472263@gmail.com>
2024-11-14 23:43:23 +0700

    fix_qr_code

--- lib/screens/platform_fee/qr_code_platform.dart
+++ lib/screens/platform_fee/qr_code_platform.dart
@@ -1,14 +1,86 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:http/http.dart' as http;
 import 'package:ticket_resell/api/response/checkout_package_fee.dart';
+import 'package:ticket_resell/screens/explore_screen.dart';
 
 // Đảm bảo rằng bạn đã có lớp CheckoutPackageFee và nhập nó từ file của bạn
-
-class QrCodePlatformScreen extends StatelessWidget {
+class QrCodePlatformScreen extends StatefulWidget {
   final CheckoutPackageFee checkoutPackageFee;
 
   const QrCodePlatformScreen({Key? key, required this.checkoutPackageFee})
       : super(key: key);
+
+  @override
+  _QrCodePlatformScreenState createState() => _QrCodePlatformScreenState();
+}
+
+class _QrCodePlatformScreenState extends State<QrCodePlatformScreen> {
+  String? status;
+  late Timer _timer;  // Declare the Timer object
+
+  // Fetch status function
+  Future<void> fetchStatus() async {
+    Map<String, String> headers = {
+      'x-client-id': '46d20310-c620-4e3b-a687-6c363f6e21a5',
+      'x-api-key': '02f8d2e9-cbf0-4bae-a9fd-7d7d6c57537e',
+    };
+    final response = await http.get(Uri.parse(
+        'https://api-merchant.payos.vn/v2/payment-requests/${widget.checkoutPackageFee.orderCode}'), headers: headers);
+
+    print(widget.checkoutPackageFee.orderCode);
+    print(response.statusCode); // For debugging
+    print(response.body); // For debugging
+    var responseData = jsonDecode(response.body);
+
+    if (responseData['code'] == "00") {
+      setState(() {
+        status = responseData['data']['status'];
+      });
+
+      print("Updated status: $status");
+
+      // If the status has changed to PAID or CANCELED, stop the polling
+      if (status == 'PAID' || status == 'CANCELED') {
+        _timer.cancel();
+        await changeStatus(widget.checkoutPackageFee.orderCode);// Stop polling if status is either PAID or CANCELED
+      }
+    } else {
+      print('Failed to load status');
+    }
+  }
+
+  Future<void> changeStatus(int orderId) async {
+    final response = await http.post(Uri.parse(
+        'https://ticketresellapi-ckhsduaycsfccjek.eastasia-01.azurewebsites.net/api/Transaction/update-status-transaction?orderCode=$orderId&status=SUCCESS'));
+
+    print(widget.checkoutPackageFee.orderCode);
+    print(response.statusCode); // For debugging
+    print(response.body); // For debugging
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Start polling when the screen is loaded
+    _timer = Timer.periodic(Duration(seconds: 2), (timer) {
+      // Only fetch status if status is not 'PAID' or 'CANCELED'
+      if (status != 'PAID' && status != 'CANCELED') {
+        fetchStatus();  // Load lại màn hình sau 2 giây
+      } else {
+        _timer.cancel();  // Stop polling if status is 'PAID' or 'CANCELED'
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();  // Make sure to cancel the timer when the screen is disposed
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,67 +89,79 @@ class QrCodePlatformScreen extends StatelessWidget {
         title: const Text("QR Code Details"),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 20),
-            // Hiển thị mã QR
-            QrImageView(
-              data: checkoutPackageFee.qrCode, // Sử dụng chuỗi QR từ đối tượng
-              version: QrVersions.auto,
-              size: 200.0,
-            ),
-            const SizedBox(height: 20),
-            // Thông tin bổ sung về giao dịch
-            Text(
-              "Order Code: ${checkoutPackageFee.orderCode}",
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "Amount: ${checkoutPackageFee.amount.toString()} VND",
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "Status: ${checkoutPackageFee.status}",
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "Payment Link ID: ${checkoutPackageFee.paymentLinkId}",
-              style: const TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 10),
-            // Link Checkout
-            ElevatedButton(
-              onPressed: () {
-                // Mở URL thanh toán nếu người dùng muốn
-                // Sử dụng phương pháp mở URL bằng trình duyệt hoặc in-app browser
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green,
-              ),
-              child: const Text(
-                "Go to Checkout",
-                style: TextStyle(
-                  color: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "If you have paid money for the platform, \nyou must wait for the status to change\nbefore you can go back or continue.",
+                  textAlign: TextAlign.center,  // Căn giữa văn bản
+                  style: const TextStyle(
+                    fontSize: 20,  // Tăng kích thước chữ để dễ đọc
+                    fontWeight: FontWeight.bold,  // Làm đậm văn bản
+                    color: Colors.black,  // Chọn màu chữ cho dễ nhìn
+                    height: 1.5,  // Tăng khoảng cách dòng để văn bản không bị chồng lên nhau
+                  ),
                 ),
-              ),
+                QrImageView(
+                  data: widget.checkoutPackageFee.qrCode,
+                  version: QrVersions.auto,
+                  size: 200.0,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  "Order Code: ${widget.checkoutPackageFee.orderCode}",
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  "Price: ${widget.checkoutPackageFee.amount.toString()} VND",
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  "Status: ${status ?? widget.checkoutPackageFee.status}",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: status == 'PAID'
+                        ? Colors.green
+                        : (status == 'CANCELED' ? Colors.red : Colors.blue),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ExploreScreen(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                  ),
+                  child: const Text(
+                    "Back to Home",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
